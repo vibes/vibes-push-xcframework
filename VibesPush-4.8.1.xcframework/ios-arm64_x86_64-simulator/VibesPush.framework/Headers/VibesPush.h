ad07df2ad6a64583a5795ea2d91021a46620a930
Clement Wafula <clement.wafula@vibes.com>
2023-05-30 14:32:16 +0300

    4.8.1

---
+++ VibesPush-4.8.1.xcframework/ios-arm64_x86_64-simulator/VibesPush.framework/Headers/VibesPush.h
@@ -0,0 +1,17 @@
+//
+//  VibesPush.h
+//  VibesPush
+//
+//  Created by Ed Lafoy on 04/10/16.
+//  Copyright © 2017 Table XI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VibesPush.
+FOUNDATION_EXPORT double VibesPushVersionNumber;
+
+//! Project version string for VibesPush.
+FOUNDATION_EXPORT const unsigned char VibesPushVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VibesPush/PublicHeader.h>
